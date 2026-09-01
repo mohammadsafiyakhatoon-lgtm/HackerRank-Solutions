704adf6b55e10c6ec15e1a27b599656b93ed0f0a
mohammadsafiyakhatoon-lgtm <mohammadsafiyakhatoon@gmail.com>
2026-09-01 21:21:25 +0530

    Create Interchanging of numbers in an array.py

---
+++ Module-1/Interchanging of numbers in an array.py
@@ -0,0 +1,6 @@
+n = int(input())
+arr = list(map(int,input().split()))
+min_index=arr.index(min(arr))
+max_index=arr.index(max(arr))
+arr[min_index],arr[max_index]=arr[max_index],arr[min_index]
+print(*arr)
